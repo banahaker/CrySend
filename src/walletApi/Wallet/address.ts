10fd89387f0943413d89504e23f33afd33a1a8ad
banahaker <pn0818x@outlook.com>
2022-05-08 11:21:30 +0800

    feat: connect to wallet imp

--- src/walletApi/Wallet/address.ts
+++ src/walletApi/Wallet/address.ts
@@ -3,10 +3,7 @@ import type { MetaMaskProvider } from "./connect";
 
 export const getAddress = async () => {
   const provider = (await detectEthereumProvider()) as MetaMaskProvider;
-  try {
-    const accounts = await provider.request({ method: "eth_accounts" });
-    return accounts;
-  } catch (err) {
-    return err;
-  }
+
+  const accounts = await provider.request({ method: "eth_accounts" });
+  return accounts;
 };
