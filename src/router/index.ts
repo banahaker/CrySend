302cf26ce3f824495c8d9d993a8a62720d039b2b
banahaker <pn0818x@outlook.com>
2022-05-08 13:59:19 +0800

    feat: deploy router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
-    {
-        path: '/',
-        component: () => import('../views/transfer.pg.vue')
-    },
-    {
-        path: '/to/:id',
-        component: () => import('../views/transferTo.pg.vue')
-    }
-]
+  {
+    path: "/",
+    component: () => import("../views/transfer.pg.vue"),
+  },
+  {
+    path: "/to/:id",
+    component: () => import("../views/transferTo.pg.vue"),
+  },
+];
 
 export default createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHashHistory(),
+  routes,
+});
